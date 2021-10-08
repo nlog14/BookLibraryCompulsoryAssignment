9d19286410c6932d9a9aabe6021d0e7bf79e057d
Nohely <ngedeon14@yahoo.com>
2021-10-08 20:12:26 +0200

    Add project files.

---
+++ BookLibraryCompulsoryAssignment/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BookLibraryCompulsoryAssignment
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get;set; }
+        public int NumberOfPages { get; set; }
+        public string ISBN13 { get; set; }
+
+        public Book(string title, string author, int numberOfPages, string isbn13)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+            ISBN13 = isbn13;
+        }
+    }
+}
